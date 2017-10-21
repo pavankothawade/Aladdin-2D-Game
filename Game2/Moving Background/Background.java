768c4cefd98ec0eb668ac5992b437606fab3215e
Pavan Kothawade <pavan.kothawade@sjsu.edu>
2017-10-20 22:50:48 -0700

    Scrolling background example

--- Game2/Moving Background/Background.java
+++ Game2/Moving Background/Background.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Background extends Actor
 {
+
     /**
      * Act - do whatever the Background wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
