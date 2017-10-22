8eb8984dbf23acf7f5e38ded62e56f3616d9f1b6
Amita Mathkar <amita.mathkar@sjsu.edu>
2017-10-21 21:44:32 -0700

    Sample background music added

--- Game2/GameOver.java
+++ Game2/GameOver.java
@@ -15,5 +15,6 @@ public class GameOver extends Actor
     public void act() 
     {
         // Add your action code here.
+        
     }    
 }
---
+++ Game2/Music.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Music here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Music extends Actor
+{
+    GreenfootSound myMusic=new GreenfootSound("Game_of_Thrones(HBO).mp3");
+    boolean firstTurn=true;
+    /**
+     * Act - do whatever the Music wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if(firstTurn)
+        {
+            myMusic.playLoop();
+            firstTurn=false;
+        }
+        if(Greenfoot.mouseClicked(this))
+        {
+            if(myMusic.isPlaying())
+            {
+            myMusic.pause();
+            setImage("play.png");
+            }
+            else
+            {
+                myMusic.playLoop();
+                setImage("pause.png");
+            }
+        }
+        if(Greenfoot.)
+    }    
+}
