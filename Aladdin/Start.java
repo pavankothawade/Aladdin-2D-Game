5b049e0af45c37b697f0d2a800180f6baf06ed29
Pritam Meher <pritamkachardas.meher@sjsu.edu>
2017-10-26 13:49:23 -0700

    Title page

---
+++ Aladdin/Start.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Start here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Start extends Actor
+{
+    /**
+     * Act - do whatever the Start wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+      //setImage(new GreenfootImage("Collect more than 15 grabbers to complete level 1 and avoid the hurdles : ", 22, Color.WHITE, Color.BLACK));
+        if (Greenfoot.mouseClicked(this)) {
+        Greenfoot.setWorld(new AladdinWorld());
+        //setImage(new GreenfootImage("Collect more than 15 grabbers to complete level 1 and avoid the hurdles : ", 22, Color.WHITE, Color.BLACK));
+    }
+    }    
+}
---
+++ Aladdin/Title.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends World
+{
+
+    /**
+     * Constructor for objects of class Title.
+     * 
+     */
+    public Title()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 700, 1); 
+        Greenfoot.start();
+        Start S1 = new Start();
+        addObject(S1,200,getHeight()/2-200);
+        
+    }
+    public void act()
+    {
+        //if (Greenfoot.isKeyDown("space")) Greenfoot.setWorld(new AladdinWorld());
+         //if(Greenfoot.isMouseClick(This())) Greenfoot.setWorld(new AladdinWorld());public void act() 
+         
+}
+//setImage(new GreenfootImage("Start Game : ", 22,Color.WHITE, Color.BLACK));
+    //new GreenfootImage("Score : ", 22, Color.WHITE, Color.BLACK);
+
+
+}
